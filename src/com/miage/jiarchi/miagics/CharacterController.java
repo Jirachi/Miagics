6b07bc64e796bb4fdb656d89acbfb25cc970adef
Aethia <elise.richard@live.fr>
2012-11-01 01:32:21 +0100

    CharacterController.java un peu avancé (renderAll et ajout dans la liste des characters ?)

--- src/com/miage/jiarchi/miagics/CharacterController.java
+++ src/com/miage/jiarchi/miagics/CharacterController.java
@@ -1,16 +1,38 @@
 package com.miage.jiarchi.miagics;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class CharacterController {
 	protected static CharacterController mSingleton;
+	protected List<Character> mCharacters;
 	protected Player mSelf;
 	
 	
+	public CharacterController(){
+		mCharacters = new ArrayList<Character>();
+		mSingleton=null;
+		mSelf=null;
+		
+	}
 	
 	
 	public Player getSelf(){
 		return mSelf;
 	}
 	
+	public void renderAll(SpriteBatch mBatch, Camera mCamera){
+		for(int i=0;i<mCharacters.size();i++){
+			mCharacters.get(i).render(mBatch, mCamera);
+		}
+	}
 	
+	public void addCharacter(Character mChara){
+		mCharacters.add(mChara);		
+	}	
 	
 }
