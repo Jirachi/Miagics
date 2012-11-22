dc5abc50069e209f6acd80e608433e35c954374b
XpLoDWilD <xplod@ouverta.fr>
2012-11-22 13:09:35 +0100

    :)

---
+++ src/com/miage/jirachi/miagics/NetworkController.java
@@ -0,0 +1,36 @@
+package com.miage.jirachi.miagics;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class NetworkController {
+    private static NetworkController mSingleton = null;
+    private Socket mSocket;
+    private NetworkThread mThread;
+    
+    
+    public static NetworkController getInstance() {
+        if (mSingleton == null) {
+            mSingleton = new NetworkController();
+        }
+        
+        return mSingleton;
+    }
+    
+    public NetworkController() {
+       
+    }
+    
+    public void connect(String ip, int port) throws IOException {
+        mSocket = new Socket(ip, port);
+        mSocket.setTcpNoDelay(true);
+        mSocket.setOOBInline(true);
+        
+        mThread = new NetworkThread(mSocket);
+        mThread.start();
+    }
+    
+    public void update() {
+        
+    }
+}
