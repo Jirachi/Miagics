08782825d693662727ebfa31a3c3879acdf51e2d
xplodwild <xplodgui@gmailcom>
2012-12-08 19:40:21 +0100

    Network: J'arriverai pas a faire mieux avec cette connerie lente de java

--- src/com/miage/jirachi/miagics/Player.java
+++ src/com/miage/jirachi/miagics/Player.java
@@ -6,7 +6,7 @@ import com.miage.jirachi.resource.ResourceAnimated;
 public class Player extends Character {
     // == Attributs
     // Temps entre chaque synchronisation de position avec le serveur
-    public final static float POS_SYNC_DELAY = 0.5f;
+    public final static float POS_SYNC_DELAY = 0.3f;
     
     protected float mTimeSincePosSync = 0;
 
@@ -24,6 +24,11 @@ public class Player extends Character {
 			if (mMoveDirection != direction) {
 				Packet packet = PacketMaker.makeMovePacket((short)direction);
 				NetworkController.getInstance().send(packet);	
+			}
+			
+			if (mMoveDirection == Character.MOVE_NOT) {
+				// On ne bouge pas, Lag compensation => on indique notre position
+				doSyncPosition();
 			}
 		}
 		
@@ -46,11 +51,15 @@ public class Player extends Character {
 	    mTimeSincePosSync += timeDelta;
 	    
 	    if (mTimeSincePosSync >= POS_SYNC_DELAY && this == CharacterController.getInstance().getSelf()) {
-	        Packet packet = PacketMaker.makeSyncPosition(getRawPosition().x, getRawPosition().y);
-	        NetworkController.getInstance().sendUnreliable(packet);
+	        doSyncPosition();
 	        mTimeSincePosSync = 0.0f;
 	    }
 	    
 	    super.act(timeDelta);
+	}
+	
+	private void doSyncPosition() {
+		Packet packet = PacketMaker.makeSyncPosition(getRawPosition().x, getRawPosition().y);
+        NetworkController.getInstance().sendUnreliable(packet);
 	}
 }
